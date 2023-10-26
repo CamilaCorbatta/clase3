c2745aee17ed7a1fdb65a2a23bf6a5afa9404bd9
CamilaCorbatta <144745240+CamilaCorbatta@users.noreply.github.com>
2023-10-26 17:41:54 -0300

    Add files via upload

---
+++ js/javas.js
@@ -0,0 +1,18 @@
+function datos() {alert("Camila Corbatta, Gmail: cami24704@gmail.com")
+}
+function cambiarfondo() {
+        document.body.style.backgroundColor= "black";
+        document.getElementById("body").style.background="black";
+        document.getElementById("main").style.backgroundColor = "black";
+        document.getElementById("main").style.color="white";
+        document.getElementById("nav").style.backgroundColor="black";
+        document.getElementById("header").style.backgroundColor = "black";
+        document.getElementById("header").style.color = "white";
+        document.getElementById("section").style.backgroundColor = "black";
+        document.getElementById("section").style.color = "whitesmoke";
+        document.getElementById("aside2").style.backgroundColor = "black";
+        document.getElementById("aside2").style.color="white";
+        document.getElementById("footer").style.backgroundColor = "black";
+        document.getElementById("footer").style.color="white";
+        document.getElementById("section1").style.backgroundColor="black";
+    }
